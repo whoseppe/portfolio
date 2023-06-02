3d4bafdd053c6eebee8051bf1971f8bd31cd9f57
Giuseppe Mele <whoseppe@gmail.com>
2023-06-02 07:53:52 -0700

    default config

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
-module.exports = async (phase) => {
-  /** @type {import('next').NextConfig} */
-  const nextConfig = {
-    reactStrictMode: false,
-    swcMinify: true,
-  };
-
-  const defaultConfig = {};
-  return withPlugins([], nextConfig)(phase, { defaultConfig });
-  // return withPlugins([], nextConfig)(phase, { undefined }); // also works
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: false,
 };
+
+module.exports = nextConfig;

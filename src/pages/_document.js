152b502ec52138929e17b64eb6c91693533b8fbc
Giuseppe Mele <whoseppe@gmail.com>
2023-06-02 08:57:25 -0700

    update favicon

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,8 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <link rel="shortcut icon" href="/static/favicon.ico" />
+      <Head>
+        <link rel="shortcut icon" href="/favicon.ico" />
+      </Head>
       <body>
         <Main />
         <NextScript />
